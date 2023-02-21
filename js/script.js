15e133e96d0367e6670354b207154bcd46412cca
Leandro Cabral Santana <100740901+Leandrocabralsantana@users.noreply.github.com>
2023-02-21 15:57:33 -0300

    first-look-page
    First try to my homepage for Argentina Programa.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('boton').onclick = function(){
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML="Probando el primer evento en JS";
+}

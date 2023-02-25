d7fb7d4942f676d217bb85663a2ae7663d2759af
Leandro Cabral Santana <100740901+Leandrocabralsantana@users.noreply.github.com>
2023-02-25 16:24:10 -0300

    Style-switcher & dark-light mode done
    Done all skins (css and js) and the light and dark mode in js, also there's some new animations on the typing screen.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-
-document.getElementById('boton').onclick = function(){
-    console.log("Capturamos el evento click");
-    document.getElementById("demo").innerHTML="Probando el primer evento en JS";
-}
+/* ///////////////////// Animacion de tipeo /////////////// */
+var typed = new Typed(".typing",{
+    strings:["programador en C", "desarrollador web trainee", "data science trainee", "programador en C", "diseñador UI/UX (también se repara calzado)"],
+    typeSpeed: 100,
+    Backspeed: 60,
+    loop: true
+})

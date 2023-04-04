47bce44bd11445044bf447d1061de67c10af346c
Leandro Cabral Santana <100740901+Leandrocabralsantana@users.noreply.github.com>
2023-04-03 21:49:53 -0300

    Repository links updated
    New images and repository links updated to github now

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ///////////////////// Animacion de tipeo /////////////// */
 var typed = new Typed(".typing",{
-    strings:["programador en C", "desarrollador web trainee", "data science trainee", "programador en C", "diseñador UI/UX (también se repara calzado)"],
+    strings:["programador en C", "desarrollador web en React.js", "desarrollador Data Science con Python ", "desarrollador en C"],
     typeSpeed: 100,
     Backspeed: 60,
     loop: true
